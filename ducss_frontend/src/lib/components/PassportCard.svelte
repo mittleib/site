756a7302ed7475ee6bfa9bbad03f61eb9109882f
James Doyle <doylej46@tcd.ie>
2025-02-18 23:23:17 +0000

    About Page Card Component

---
+++ ducss_frontend/src/lib/components/PassportCard.svelte
@@ -0,0 +1,76 @@
+<script lang="ts">
+
+    export let name : String = "Product Name";
+    export let description : String = "Lorem ipsum dolor sit amet, conse";
+    export let image : String = "https://images.unsplash.com/photo-1585533530535-2f4236949d08?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+
+</script>
+
+<div class="card">
+    <div class="image" style="background-image: url({image})"/>
+    <div class="details">
+        <h1 class="name"> {name} </h1>
+        <h2 class="description"> {description} </h2>
+    </div>
+</div>
+
+<style>
+
+    * {
+        font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    }
+
+    .card {
+        display: inline-block;
+        width: 250px;
+        height: 325px;
+        background-color: hsl(150, 100%, 98%);
+        box-shadow: 0 10px 20px rgba(0, 0, 0, 0.15), 0 3px 6px rgba(0, 0, 0, 0.10);
+        padding-top: 10px;
+        border-radius: 15px;
+        transition: box-shadow 750ms ease-in-out;
+        cursor:pointer;
+    }
+
+    .card:hover {
+        display: inline-block;
+        width: 250px;
+        height: 325px;
+        background-color: hsl(150, 100%, 98%);
+        box-shadow: 0 20px 40px rgba(0, 0, 0, 0.4);
+        padding-top: 10px;
+        border-radius: 15px;
+    }
+
+    .image {
+        background-size: cover;
+        height: 250px;
+        width: 230px;
+        margin-left: 10px;
+        border-radius: 10px;
+    }
+
+    .details {
+        padding: 0 20px;
+        margin-top: -8px;
+    }
+
+    .name {
+        font-size: 20px;
+        font-weight: 600;
+        text-align: left;
+        margin-bottom: 5px;
+        letter-spacing: -0.05em;
+        color: hsl(209, 15%, 28%);
+    }
+
+    .description {
+        font-size: 16px;
+        font-weight: 500;
+        text-align: left;
+        margin-top: -10px;
+        letter-spacing: -0.05em;
+        color: hsl(208, 12%, 58%);
+    }
+
+</style>
--- ducss_frontend/src/routes/(site)/about/+page.svelte
+++ ducss_frontend/src/routes/(site)/about/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+    //↓↓ Images ↓↓
     import Chair from "$lib/images/committee/darragh.jpg"
     import Treasurer from "$lib/images/committee/manel.jpg"
     import Secretary from "$lib/images/committee/mia.jpg"
@@ -14,86 +15,29 @@
     import eSports from "$lib/images/committee/rohn.jpg"
     import DiversityAndInclusion from "$lib/images/committee/elizabeth.jpg"
     import FirstYearRepresentative from "$lib/images/committee/zara.jpg"
+    //↑↑ Images ↑↑
+
+import PassportCard from "$lib/components/PassportCard.svelte";
 </script>
 
 <div class="about-page">
     <h1>The DUCSS Committee (2024/25)</h1>
     <div class="committee-container">
-        <div class="committee-member">
-            <h2>Chair</h2>
-            <h3>Darragh Condron</h3>
-            <img class="committee-member-image" src={Chair} alt="Chair" />
-        </div>
-        <div class="committee-member">
-            <h2>Treasurer</h2>
-            <h3>Manuel Araújo</h3>
-            <img class="committee-member-image" src={Treasurer} alt="Treasurer" />
-        </div>
-        <div class="committee-member">
-            <h2>Secretary</h2>
-            <h3>Mia Rolfe</h3>
-            <img class="committee-member-image" src={Secretary} alt="Secretary" />
-        </div>
-        <div class="committee-member">
-            <h2>PRO</h2>
-            <h3>Lucy Byrne</h3>
-            <img class="committee-member-image" src={PRO} alt="Public Relations Officer"/>
-        </div>
-        <div class="committee-member">
-            <h2>Tech Lead</h2>
-            <h3>James Doyle</h3>
-            <img class="committee-member-image" src={TechLead} alt="Tech Lead" />
-        </div>
-        <div class="committee-member">
-            <h2>Amenities</h2>
-            <h3>Madalina Costovici</h3>
-            <!-- <img class="committee-member-image" src={Treasurer} alt="Treasurer"/> -->
-        </div>
-        <div class="committee-member">
-            <h2>Competitions</h2>
-            <h3>Ross Devine</h3>
-            <img class="committee-member-image" src={Competitions} alt="Competitions" />
-        </div>
-        <div class="committee-member">
-            <h2>Gaming</h2>
-            <h3>Ralf Duli</h3>
-            <img class="committee-member-image" src={Gaming} alt="Gaming" />
-        </div>
-        <div class="committee-member">
-            <h2>Events</h2>
-            <h3>Daniel Guerin</h3>
-            <img class="committee-member-image" src={Events} alt="Events" />
-        </div>
-        <div class="committee-member">
-            <h2>CoderDojo</h2>
-            <h3>Siddhi Srivastav</h3>
-            <img class="committee-member-image" src={CoderDojo} alt="CoderDojo" />
-        </div>
-        <div class="committee-member">
-            <h2>General Officer</h2>
-            <h3>Ciara Quinn</h3>
-            <img class="committee-member-image" src={GeneralOfficer1} alt="General Officer" />
-        </div>
-        <div class="committee-member">
-            <h2>General Officer</h2>
-            <h3>Ellie Smith</h3>
-            <img class="committee-member-image" src={GeneralOfficer2} alt="General Officer" />
-        </div>
-        <div class="committee-member">
-            <h2>eSports</h2>
-            <h3>Rohn Allen Santiago</h3>
-            <img class="committee-member-image" src={eSports} alt="eSports" />
-        </div>
-        <div class="committee-member">
-            <h2>D&I</h2>
-            <h3>Elizabeth Gogova</h3>
-            <img class="committee-member-image" src={DiversityAndInclusion} alt="Diversity & Inclusion" />
-        </div>
-        <div class="committee-member">
-            <h2>First Year Rep</h2>
-            <h3>Zara Braastad-Korhonen</h3>
-            <img class="committee-member-image" src={FirstYearRepresentative} alt="First Year Representative"/>
-        </div>
+        <PassportCard name='Darragh Condron' description='Chair' image={Chair}/>
+        <PassportCard name='Manuel Araújo' description='Treasurer' image={Treasurer}/>
+        <PassportCard name='Mia Rolfe' description='Secretary' image={Secretary}/>
+        <PassportCard name='Lucy Byrne' description='PRO' image={PRO}/>
+        <PassportCard name='James Doyle' description='Tech Lead' image={TechLead}/>
+        <PassportCard name='Madalina Costovici' description='Amenities'/>
+        <PassportCard name='Ross Devine' description='Competitions' image={Competitions}/>
+        <PassportCard name='Ralf Duli' description='Gaming' image={Gaming}/>
+        <PassportCard name='Daniel Guerin' description='Events' image={Events}/>
+        <PassportCard name='Siddhi Srivastav' description='CoderDojo' image={CoderDojo}/>
+        <PassportCard name='Ciara Quinn' description='General Officer' image={GeneralOfficer1}/>
+        <PassportCard name='Ellie Smith' description='General Officer' image={GeneralOfficer2}/>
+        <PassportCard name='Rohn Allen Santiago' description='eSports' image={eSports}/>
+        <PassportCard name='Elizabeth Gogova' description='Diversity & Inclusion' image={DiversityAndInclusion}/>
+        <PassportCard name='Zara Braastad-Korhonen' description='First Year Rep' image={FirstYearRepresentative}/>
     </div>
 </div>
 
@@ -108,8 +52,13 @@
     }
 
     h1 {
-        font-size: 2.5rem;
+        text-align: center;
+        font-size: 36px;
+        font-weight: 700;
+        letter-spacing: -0.05em;
+        
         margin-bottom: 24px;
+        color: hsl(209, 15%, 20%);
     }
 
     .committee-container {
@@ -119,54 +68,9 @@
         padding: 16px;
     }
 
-    .committee-member {
-        background-color: hsla(208, 12%, 95%, 1.0);
-        border: 1px solid hsl(208, 12%, 85%);
-        border-radius: 12px;
-        padding: 24px;
-        text-align: center;
-        box-shadow: 0 6px 8px rgba(0, 0, 0, 0.15);
-    }
-
-    h2 {
-        color: hsl(35, 95%, 58%);
-        font-size: 1.5rem;
-        margin-bottom: 12px;
-    }
-
-    h3 {
-        font-size: 1.125rem;
-        margin-bottom: 20px;
-    }
-
-    .committee-member-image {
-        width: 200px;
-        height: 200px; 
-        object-fit: cover;
-        border-radius: 12px;
-        border: 3px solid hsl(35, 95%, 58%);
-    }
-
     @media (max-width: 600px) {
         h1 {
             font-size: 2.25rem;
-        }
-
-        .committee-member {
-            padding: 32px;
-        }
-
-        h2 {
-            font-size: 2.5rem;
-        }
-
-        h3 {
-            font-size: 1.5rem;
-        }
-
-        .committee-member-image {
-            width: 250px;
-            height: 250px;
         }
 
         .committee-container {
