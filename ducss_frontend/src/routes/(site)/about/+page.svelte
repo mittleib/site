2b5b6a1fb4168cc46818a274ee9fc0578aff4908
Peter Looney <plooney@tcd.ie>
2026-01-21 19:13:17 +0000

    Updated committee pics to 25/26

--- ducss_frontend/src/routes/(site)/about/+page.svelte
+++ ducss_frontend/src/routes/(site)/about/+page.svelte
@@ -1,20 +1,20 @@
 <script>
     //↓↓ Images ↓↓
-    import Chair from "$lib/images/committee/darragh.jpg"
-    import Treasurer from "$lib/images/committee/manel.jpg"
-    import Secretary from "$lib/images/committee/mia.jpg"
-    import PRO from "$lib/images/committee/lucy.jpg"
-    import TechLead from "$lib/images/committee/james.jpg"
-    // import Amenities from "$lib/images/committee/"
-    import Competitions from "$lib/images/committee/ross.jpg"
-    import Gaming from "$lib/images/committee/ralf.jpg"
-    import Events from "$lib/images/committee/dan.jpg"
-    import CoderDojo from "$lib/images/committee/siddhi.jpg"
-    import GeneralOfficer1 from "$lib/images/committee/ciara.jpeg"
-    import GeneralOfficer2 from "$lib/images/committee/ellie.png"
-    import eSports from "$lib/images/committee/rohn.jpg"
-    import DiversityAndInclusion from "$lib/images/committee/elizabeth.jpg"
-    import FirstYearRepresentative from "$lib/images/committee/zara.jpg"
+    import Chair from "$lib/images/committee/lucy.jpeg"
+    import Treasurer from "$lib/images/committee/mia.jpeg"
+    import Secretary from "$lib/images/committee/madz.jpeg"
+    import PRO from "$lib/images/committee/zara.jpeg"
+    import TechLead from "$lib/images/committee/plooney.jpeg"
+    import Amenities from "$lib/images/committee/ayushmaan.jpeg"
+    import Competitions from "$lib/images/committee/elizabeth.jpeg"
+    import Gaming from "$lib/images/committee/pavit.jpeg"
+    import Events from "$lib/images/committee/darragh.jpeg"
+    import CoderDojo from "$lib/images/committee/agnetha.jpeg"
+    import GeneralOfficer1 from "$lib/images/committee/izzy.jpeg"
+    import GeneralOfficer2 from "$lib/images/committee/darren.jpeg"
+    import eSports from "$lib/images/committee/keith.jpeg"
+    // import DiversityAndInclusion from "$lib/images/committee/elizabeth.jpg"
+    import FirstYearRepresentative from "$lib/images/committee/dimitris.jpeg"
     //↑↑ Images ↑↑
 
 import PassportCard from "$lib/components/PassportCard.svelte";
@@ -23,21 +23,21 @@ import PassportCard from "$lib/components/PassportCard.svelte";
 <div class="about-page">
     <h1>The DUCSS Committee (2025/26)</h1>
     <div class="committee-container">
-        <PassportCard name='Darragh Condron' description='Chair' image={Chair}/>
-        <PassportCard name='Manuel Araújo' description='Treasurer' image={Treasurer}/>
-        <PassportCard name='Mia Rolfe' description='Secretary' image={Secretary}/>
-        <PassportCard name='Lucy Byrne' description='PRO' image={PRO}/>
-        <PassportCard name='James Doyle' description='Tech Lead' image={TechLead}/>
-        <PassportCard name='Madalina Costovici' description='Amenities'/>
-        <PassportCard name='Ross Devine' description='Competitions' image={Competitions}/>
-        <PassportCard name='Ralf Duli' description='Gaming' image={Gaming}/>
-        <PassportCard name='Daniel Guerin' description='Events' image={Events}/>
-        <PassportCard name='Siddhi Srivastav' description='CoderDojo' image={CoderDojo}/>
-        <PassportCard name='Ciara Quinn' description='General Officer' image={GeneralOfficer1}/>
-        <PassportCard name='Ellie Smith' description='General Officer' image={GeneralOfficer2}/>
-        <PassportCard name='Rohn Allen Santiago' description='eSports' image={eSports}/>
-        <PassportCard name='Elizabeth Gogova' description='Diversity & Inclusion' image={DiversityAndInclusion}/>
-        <PassportCard name='Zara Braastad-Korhonen' description='First Year Rep' image={FirstYearRepresentative}/>
+        <PassportCard name='Lucy Byrne' description='Chair' image={Chair}/>
+        <PassportCard name='Mia Rolfe' description='Treasurer' image={Treasurer}/>
+        <PassportCard name='Madalina Costovici' description='Secretary' image={Secretary}/>
+        <PassportCard name='Braastad-Korhonen' description='PRO' image={PRO}/>
+        <PassportCard name='Plooney' description='Tech Lead' image={TechLead}/>
+        <PassportCard name='Elizabeth Gogová' description='Competitions' image={Competitions}/>
+        <PassportCard name='Pavit Gogia' description='Gaming' image={Gaming}/>
+        <PassportCard name='Darragh Condron' description='Events' image={Events}/>
+        <PassportCard name='Agnetha Kamath' description='CoderDojo' image={CoderDojo}/>
+        <PassportCard name='Izzy Guey' description='General Officer' image={GeneralOfficer1}/>
+        <PassportCard name='Darren McCabe' description='General Officer' image={GeneralOfficer2}/>
+        <PassportCard name='Keith Lyons' description='eSports' image={eSports}/>
+        <!-- <PassportCard name='' description='Diversity & Inclusion' image={DiversityAndInclusion}/> -->
+        <PassportCard name='Ayushmaan Kumar Yadav' description='Amenities' image={Amenities}/>
+        <PassportCard name='Dmitiris Charalampakis' description='First Year Rep' image={FirstYearRepresentative}/>
     </div>
 </div>
 
